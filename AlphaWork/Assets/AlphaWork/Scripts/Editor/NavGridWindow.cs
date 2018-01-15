1dfad4fee65b9196e8010db44adb34099daf14a3
myl2232 <153101862@qq.com>
2018-01-15 22:26:41 +0800

    增加控件变量

--- AlphaWork/Assets/AlphaWork/Scripts/Editor/NavGridWindow.cs
+++ AlphaWork/Assets/AlphaWork/Scripts/Editor/NavGridWindow.cs
@@ -9,8 +9,20 @@ public class NavGridWindow : EditorWindow {
     private NavGridTool m_gridTool;
     Vector3 ptStart;
     Vector3 ptEnd;
+    float m_brushSize = 0.0f;
+    float m_viewSize = 0.0f;
+    float m_meshSize = 0.0f;
+    float m_fMeshSize = 0.0f;
+    bool m_bAccurateHight = false;
+    float m_HeightField = 0.0f;
+    bool m_bOpenBlock = true;
+    bool m_bCloseBlock = false;
+    bool m_bFlushHeight = false;
+    Vector3 m_StartPt = new Vector3();
+    Vector3 m_EndPt = new Vector3();
+
     //通过MenuItem按钮来创建这样的一个对话框  
-    [MenuItem("Window/NavGridWindow")]
+    [MenuItem("Window/NavGridWindow",false,1)]
     public static void ConfigDialog()
     {
         EditorWindow.GetWindow(typeof(NavGridWindow));
@@ -33,27 +45,27 @@ public class NavGridWindow : EditorWindow {
         GUILayout.BeginArea(new Rect(10, 10, 300, 1000));
         EditorGUILayout.BeginHorizontal();
         GUILayout.Label("画刷大小：", EditorStyles.boldLabel);
-        GUILayout.HorizontalSlider(1, 0, 10);
+        m_brushSize = GUILayout.HorizontalSlider(1, 0, 10);
         EditorGUILayout.DelayedIntField(1, GUILayout.Width(50));
         EditorGUILayout.EndHorizontal();
         EditorGUILayout.BeginHorizontal();
         GUILayout.Label("可视范围：", EditorStyles.boldLabel);
-        GUILayout.HorizontalSlider(1, 0, 10);
+        m_viewSize = GUILayout.HorizontalSlider(1, 0, 10);
         EditorGUILayout.DelayedIntField(1, GUILayout.Width(50));
         EditorGUILayout.EndHorizontal();
         EditorGUILayout.BeginHorizontal();
         GUILayout.Label("网格大小：", EditorStyles.boldLabel);
-        GUILayout.HorizontalSlider(1, 0, 10);
-        EditorGUILayout.DelayedIntField(1, GUILayout.Width(50));
+        m_meshSize = GUILayout.HorizontalSlider(1, 0, 10);
+        m_fMeshSize = EditorGUILayout.DelayedFloatField(1.0f, GUILayout.Width(50));
         EditorGUILayout.EndHorizontal();
         GUILayout.EndArea();
-        GUILayout.Box(GUIContent.none, GUILayout.ExpandWidth(true), GUILayout.Height(1f));
-        
+        //GUILayout.Box(GUIContent.none, GUILayout.ExpandWidth(true), GUILayout.Height(1f));
+        EditorGUILayout.Space();
         GUILayout.BeginArea(new Rect(10, 80, 300, 1000));
-        EditorGUILayout.Toggle("启动精准高度", false);
+        m_bAccurateHight = EditorGUILayout.Toggle("启动精准高度", false);
         EditorGUILayout.BeginHorizontal();
         GUILayout.Label("自地面以上射线高度：", EditorStyles.boldLabel);
-        GUILayout.HorizontalSlider(1, 0, 100);
+        m_HeightField = GUILayout.HorizontalSlider(1, 0, 100);
         EditorGUILayout.DelayedIntField(1, GUILayout.Width(50));
         EditorGUILayout.EndHorizontal();
         if (GUILayout.Button("生成网格", GUILayout.Width(300)))
@@ -65,10 +77,10 @@ public class NavGridWindow : EditorWindow {
         GUILayout.BeginArea(new Rect(10, 140, 300, 500));
         GUILayout.Label("基本操作：", EditorStyles.boldLabel);
         EditorGUILayout.BeginVertical();
-        EditorGUILayout.Toggle("开启通路", true);
-        EditorGUILayout.Toggle("关闭通路", false);
+        m_bOpenBlock = EditorGUILayout.Toggle("开启通路", true);
+        m_bCloseBlock = EditorGUILayout.Toggle("关闭通路", false);
         EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.Toggle("刷新Z值", false);
+        m_bFlushHeight =EditorGUILayout.Toggle("刷新Z值", false);
         GUILayout.HorizontalSlider(1, 0, 10);
         EditorGUILayout.DelayedIntField(1, GUILayout.Width(50));
         EditorGUILayout.EndHorizontal();
@@ -82,14 +94,14 @@ public class NavGridWindow : EditorWindow {
         {
             SetStartPt();
         }
-        EditorGUILayout.Vector3Field("",ptStart);
+        m_StartPt = EditorGUILayout.Vector3Field("",ptStart);
         EditorGUILayout.EndHorizontal();
         EditorGUILayout.BeginHorizontal();
         if (GUILayout.Button("终止点", GUILayout.Width(120)))
         {
             SetEndPt();
         }
-        EditorGUILayout.Vector3Field("", ptStart);
+        m_EndPt = EditorGUILayout.Vector3Field("", ptStart);
         EditorGUILayout.EndHorizontal();
         GUILayout.EndArea();
 
@@ -133,9 +145,13 @@ public class NavGridWindow : EditorWindow {
     }
 
     private void OnInspectorUpdate()
-    {
-        
+    {        
+        Repaint();
     }
 
+    private void OnInspectorGUI()
+    {
+
+    }
 
 }
