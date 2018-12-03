1f268e46fe3b15a80ae14cb81f11b579d459cb1e
myl2232 <153101862@qq.com>
2018-12-03 21:05:37 +0800

    有修改而已

--- LuaScripts/Game/InGame/ui/UIInGame.lua
+++ LuaScripts/Game/InGame/ui/UIInGame.lua
@@ -6,12 +6,32 @@ local UIInGame = Lplus.Extend(TBPanelBase, "UIInGame");
 local def = UIInGame.define;
 local m_Instance = nil;
 
+--类成员
+def.field("userdata").mAttack1 = nil;
+def.field("userdata").mAttack2 = nil;
+def.field("userdata").mAddItem = nil;
+def.field("userdata").mAI = nil;
+def.field("userdata").mExit = nil;
+def.field("userdata").mReplace = nil;
+def.field("userdata").mTrackPad = nil;
+
 -- 单例对象
 def.static("=>", UIInGame).Instance = function (self)
 	if(m_Instance == nil) then
 		m_Instance = UIInGame();
+		print("=====================intstance UIInGame ")
 	end
 	return m_Instance;
+end
+
+def.override().OnCreate = function (self)
+    self.mAttack1 = self:FindChild("Attack1")
+    self.mAttack2 = self:FindChild("Attack2")
+    self.mAddItem = self:FindChild("AddBtn")
+	self.mAI = self:FindChild("AIBtn")
+	self.mExit = self:FindChild("btnExit")
+	self.mReplace = self:FindChild("ReplaceBtn")
+	self.mTrackPad = self:FindChild("TrackPad")
 end
 
 -- 预创建UI回调
@@ -19,7 +39,25 @@ def.override().DoCreate = function (self)
 	--self:CreateUGUIPanel(GameUtil.GetResPath(100002), 3,{});
 end
 
-function UIInGame.RegistObj( self, obj )
+-- 单击事件回调
+def.method("userdata").onClickObj = function(self, obj)
+    if(obj.name == self.mAttack1.name) then
+        self:OnAttack1(obj);
+    elseif(obj.name == self.mAttack2.name) then    
+        self:OnAttack2(obj);
+        
+    end
+end
+
+def.method("userdata").OnAttack1 = function ( self, obj )
+
+end
+
+def.method("userdata").OnAttack2 = function ( self, obj )
+
+end
+
+function UIInGame.RegistObj( obj )
     -- body	
 	print("------------------Regist Panel----------------"..obj.name..".................")
 	m_Instance:CreateUGUIPanel(GameUtil.GetResPath(100002), 1,{},obj);
--- LuaScripts/Game/Login/ui/UILogin.lua
+++ LuaScripts/Game/Login/ui/UILogin.lua
@@ -10,6 +10,7 @@ def.field("userdata").mInputUser = nil;
 def.field("userdata").mLoginBtn = nil;
 def.field("userdata").mLogoutBtn = nil;
 def.field("userdata").mUserList = nil;
+def.field("userdata").mUserText = nil;
 
 -- 单例对象
 def.static("=>", UILogin).Instance = function (self)
@@ -25,6 +26,7 @@ def.override().OnCreate = function (self)
     self.mLoginBtn = self:FindChild("Canvas/BtnLogin")
     self.mLogoutBtn = self:FindChild("Canvas/BtnExit")
     self.mUserList = self:FindChild("Canvas/UserViewList")
+    self.mUserText = self:FindChild("Canvas/InputField/Text")    
 end
 
 -- 预创建UI回调
@@ -39,18 +41,45 @@ def.method("userdata").onClickObj = function(self, obj)
         self:OnClickLogin(obj);
     elseif(obj.name == self.mLogoutBtn.name) then    
         self:OnClickLogout(obj);
+    elseif(obj.name == self.mUserList.name) then    
+        self:OnClickUserList(obj);
     end
 end
 
 def.method("userdata").OnClickLogin = function ( self, obj )
     -- body
-    self.m_panel:GetComponent("UGUIFormExtend"):ProcedureImpl()
+    if self.mUserText:GetComponent("InputField").text ~= "" then
+        self:CreateUserImpl(self.mUserText.text);
+        self.m_panel:GetComponent("UGUIFormExtend"):ProcedureImpl()
+    end    
 end
 
 def.method("userdata").OnClickLogout = function ( self, obj )
     -- body
     local arg = AlphaWork.GameToLoginEventArgs.New();--Myl:调用方式有疑问
     AlphaWork.GameEntry.Event:Fire(this, arg);
+end
+
+def.method("userdata").OnClickUserList = function ( self, obj )
+    -- body
+    print("--------------clicked userlist---------")
+    --self.m_panel:GetComponent("UGUIFormExtend"):ProcedureImpl()
+end
+
+def.method("string").CreateUserImpl = function ( self, name )
+    local player = UPlayer().New();
+    player.user = name;
+    player.gamesetting = GameEntry.Config.GameSetting.UID;
+
+    local players = {};
+    players = AlphaWork.GameEntry.DataBase.GetPlayerByName(name);
+    --[[ AlphaWork.GameEntry.DataBase.DataDevice.GetDataByKey<AlphaWork.UPlayer>(name,out players);]]
+    if players == nil or players[0] == nil then
+        AlphaWork.GameEntry.DataBase.AddPlayer(player);
+    else
+        players[0].gamesetting = player.gamesetting;
+    end 
+    AlphaWork.GameEntry.Config.GameSetting.CurrentUser = name;    
 end
 
 function UILogin.RegistObj( obj )
